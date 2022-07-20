0867df984becc6609824aa687b88bc9fd1cfb054
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-20 14:14:57 -0300

    pokemon cards v1

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,9 +1,33 @@
 import React from "react";
 
 const PokemonCard = (props) => {
-  const { pokemon, loading } = props;
+  const { pokemon } = props;
   return(
-    <div>aaa</div>
+    <div className="pokemon-card">
+      <div className="pokemon-image-container">  
+        <img 
+          alt={pokemon.name} 
+          src={pokemon.sprites.front_default}
+          className="pokemon-image"
+        />
+      </div>
+      <div className="pokemon-card-title">
+        <h3>{pokemon.name}</h3>
+        <span># {pokemon.id}</span>
+      </div>
+      <div className="pokemon-card-body">
+        <div className="pokemon-type">
+          {pokemon.types.map((item, index) => {
+            return (
+              <div key={index} className="pokemon-type-text">{item.type.name}</div>
+            )
+          })}
+        </div>
+        <div className="pokemon-aditional-info">
+
+        </div>
+      </div>
+    </div>
   );
 }
 

7cb1685bad8e1ee54760115d52951cc7e4c27d9d
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-20 10:32:49 -0300

    use states to manipulate pokedex

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { listPokemon } from './assets/api';
 import Navbar from './components/Navbar';
 import Pokedex from './components/Pokedex';
 
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemonList, setPokemon] = useState([]);
+  const fetchPokemon = async () => {
+    try {
+      setLoading(true)
+      const result = await listPokemon();
+      setPokemon(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error (fetchPokemon): ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  }, [pokemonList]);
+
   return (
     <div>
       <Navbar />
       <div className="App">
-        <Pokedex />
+        <Pokedex pokemonList = {pokemonList} loading = {loading} />
       </div>
     </div>
   );

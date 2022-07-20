ef18ffd0b663fa6f9f1500d5ae3986c5a155902b
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-20 09:59:17 -0300

    pokedex setup

---
+++ src/components/Pokedex.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pokedex = () => {
+  return (
+    <div>
+      <div className="pokedex-header">
+        <h1>Pokédex</h1>
+      </div>
+      <div>AAAA</div>
+    </div>
+  )
+}
+
+export default Pokedex;

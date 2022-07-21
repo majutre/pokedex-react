7a8131f09b06d3e32387021b4833f82de6d0844f
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-21 15:49:18 -0300

    add default image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,35 +4,53 @@ import { GiBodyHeight } from "react-icons/gi";
 
 const PokemonCard = (props) => {
   const { pokemon } = props;
-  let pokemonWeightInKg = (pokemon.weight * 0.1).toFixed(1);
-  let pokemonHeightInMeters = (pokemon.height * 0.1).toFixed(1);
+  const weightInKg = (pokemon.weight * 0.1).toFixed(1);
+  const heightInMeters = (pokemon.height * 0.1).toFixed(1);
+  const imageMissing = pokemon.sprites.front_default == null;
+  const imagePath = imageMissing ? "./which_pkmn.jpeg" : pokemon.sprites.front_default;
+  const extendImage = imageMissing ? " pokemon-card-extend-image" : "";
 
   return (
     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 pokemon-card">
-      <div className={"pokemon-image-container background-" + pokemon.types[0].type.name}>
+      <div
+        className={
+          "pokemon-image-container background-" + pokemon.types[0].type.name
+        }
+      >
+        {console.log(pokemon.sprites.front_default)}
         <img
           alt={pokemon.name}
-          src={pokemon.sprites.front_default}
-          className="pokemon-image"
+          src={imagePath}
+          className={"pokemon-image" + extendImage}
         />
       </div>
       <div className="pokemon-card-title mt-3">
         <h3>{pokemon.name}</h3>
-        <span># {pokemon.id}</span>
+        <h6># {pokemon.id}</h6>
       </div>
       <div className="pokemon-card-body">
         <div className="pokemon-type">
           {pokemon.types.map((item, index) => {
             return (
-              <div key={index} className={"px-2 pokemon-type-text background-" + item.type.name}>
+              <div
+                key={index}
+                className={
+                  "px-2 pokemon-type-text background-" + item.type.name
+                }
+              >
                 {item.type.name}
               </div>
             );
           })}
         </div>
-        <div className="pokemon-aditional-info">
-          <p className="mb-0 mt-1"><GiBodyHeight className="mb-1 mx-2"/>{pokemonHeightInMeters} m</p>
-          <p><FaWeightHanging className="mb-1 mx-1"/> {pokemonWeightInKg} kg</p>
+        <div className="pokemon-aditional-info mt-3">
+          <p className="mb-0 mt-1">
+            <GiBodyHeight className="mb-1 mx-2" />
+            {heightInMeters} m
+          </p>
+          <p>
+            <FaWeightHanging className="mb-1 mx-1" /> {weightInKg} kg
+          </p>
         </div>
       </div>
     </div>

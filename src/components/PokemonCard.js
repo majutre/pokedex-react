a958c458db977b113a3d1e17a68f8a2c379863ee
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-07-22 09:32:39 -0300

    wip - profile modal

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaWeightHanging } from "react-icons/fa";
 import { GiBodyHeight } from "react-icons/gi";
+import PokemonProfileModal from "./PokemonProfileModal";
 
 const PokemonCard = (props) => {
   const { pokemon } = props;
+  const [show, setShow] = useState(false)
   const weightInKg = (pokemon.weight * 0.1).toFixed(1);
   const heightInMeters = (pokemon.height * 0.1).toFixed(1);
   const imageMissing = pokemon.sprites.front_default == null;
   const imagePath = imageMissing ? "./which_pkmn.jpeg" : pokemon.sprites.front_default;
   const extendImage = imageMissing ? " pokemon-card-extend-image" : "";
 
+  // const onButtonClickHandler = () => {
+  //   render(<PokemonProfileModal />) 
+  // }
+
   return (
     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 pokemon-card">
+        <h6 className="text-end mb-3"># {pokemon.id} </h6>
       <div
         className={
           "pokemon-image-container background-" + pokemon.types[0].type.name
         }
       >
-        {console.log(pokemon.sprites.front_default)}
+
         <img
           alt={pokemon.name}
           src={imagePath}
@@ -26,7 +33,6 @@ const PokemonCard = (props) => {
       </div>
       <div className="pokemon-card-title mt-3">
         <h3>{pokemon.name}</h3>
-        <h6># {pokemon.id}</h6>
       </div>
       <div className="pokemon-card-body">
         <div className="pokemon-type">
@@ -49,9 +55,13 @@ const PokemonCard = (props) => {
             {heightInMeters} m
           </p>
           <p>
-            <FaWeightHanging className="mb-1 mx-1" /> {weightInKg} kg
+            <FaWeightHanging className="mb-1 mx-1" /> 
+            {weightInKg} kg
           </p>
         </div>
+
+        <button onClick={() => setShow(true)}>CLICK ME</button>
+        <PokemonProfileModal show={show} onHide={() => setShow(false)} />
       </div>
     </div>
   );
